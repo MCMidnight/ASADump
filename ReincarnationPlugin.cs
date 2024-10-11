75c06f96f5da16a5dc348d49679ca2add81d625c
Midnight <82400448+MCMidnight@users.noreply.github.com>
2024-10-11 12:12:56 +0100

    Update ReincarnationPlugin.cs

--- ReincarnationPlugin.cs
+++ ReincarnationPlugin.cs
@@ -14,21 +14,18 @@ namespace PlutoScripts.Reincarnation
             Logger.Log($"{Name} {Assembly.GetName().Version.ToString(3)} has been loaded!");
             Logger.Log("Made by Pluto Scripts.");
             PlayerLife.onPlayerDied += OnPlayerDied;
-
             if (Configuration.Instance.DebugMode)
             {
                 Logger.Log("Debug Mode is enabled.");
                 Logger.Log($"Respawn delay is set to {Configuration.Instance.RespawnDelay} seconds.");
             }
         }
-
         protected override void Unload()
         {
             Logger.Log($"{Name} {Assembly.GetName().Version.ToString(3)} has been unloaded!");
             Logger.Log("Made by Pluto Scripts.");
             PlayerLife.onPlayerDied -= OnPlayerDied;
         }
-
         private void OnPlayerDied(PlayerLife sender, EDeathCause cause, ELimb limb, CSteamID murderer)
         {
             var player = sender.player;
@@ -37,7 +34,6 @@ namespace PlutoScripts.Reincarnation
                 AutoRespawnPlayer(player);
             }, Configuration.Instance.RespawnDelay);
         }
-
         private void AutoRespawnPlayer(Player player)
         {
             if (player != null && player.life != null && player.life.isDead)
